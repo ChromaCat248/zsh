f6a1682203a1167593a8fb14b6e03183483b0759
ChromaCat248 <chromacat248@gmail.com>
2024-03-07 17:29:22 -0500

    add shell options and environment variables for history

---
+++ zshrc.d/env.zsh
@@ -0,0 +1,2 @@
+export HISTFILE=$HOME/.config/zsh/history
+export SAVEHIST=1
--- zshrc.d/shell_options.zsh
+++ zshrc.d/shell_options.zsh
@@ -1,1 +1,8 @@
+# fs navigation
 setopt autocd
+setopt autopushd
+setopt pushd_silent
+setopt cd_silent
+
+# history
+setopt append_history
